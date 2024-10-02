deda24f8261ce87081586851d45025dc4e82ba69
mherreradiaz <m.herreradiaz16@gmail.com>
2024-10-02 14:41:38 -0300

    subir figuras de correlación general

--- script/09_analizar correlacion.R
+++ script/09_analizar correlacion.R
@@ -1,5 +1,89 @@
 source('script/00_setup.R')
 library(viridis)
+library(ggcorrplot)
+library(patchwork)
+
+# matriz de correlación entre indicadores
+
+data <- read_rds('data/processed/rds/dataset.rds')
+
+indicadores <- data |>
+  select(año:cob,DI,SWEI,everything()) |> 
+  pivot_longer(cols=DI:`zcSM-6`,names_to='indicator',values_to='value') |> 
+  pull(indicator) |> 
+  unique() |> 
+  rev()
+
+data <- data |> 
+  select(año:cob,DI,SWEI,everything()) |> 
+  pivot_longer(cols=DI:`zcSM-6`,names_to='indicator',values_to='value') |>
+  mutate(indicator = factor(indicator,levels = indicadores)) |> 
+  arrange(año,estacion,shac,cob,indicator) |> 
+  pivot_wider(names_from = 'indicator',values_from = 'value')
+
+shacs <- data$shac |> unique()
+
+estaciones <- c('Verano','Otoño','Invierno','Primavera')
+
+for (i in seq_along(shacs)) {
+  
+  data_shac <- data |> 
+    filter(shac == shacs[i])
+
+  estaciones <- data_shac$estacion |> unique()
+  
+  plot_estacion <- list()
+  
+  for (x in seq_along(estaciones)) {
+    
+    data_estacion <- data_shac |> 
+      filter(estacion == estaciones[x]) |>
+      select(-año, -estacion, -shac, -cob) |> 
+      cor(use = "complete.obs")
+    
+    plot_estacion[[x]] <- data_estacion |> 
+      ggcorrplot(method = "square", 
+                 type = "upper",
+                 lab = TRUE,
+                 lab_size = 2.2,
+                 lab_col = 'white',
+                 ggtheme = theme_bw()) +
+      geom_text(aes(Inf,-Inf),label = estaciones[x],vjust = -1,hjust = 1.1,
+                size = 5) +
+      scale_fill_viridis_c(option = "D", 
+                           limits = c(-1, 1),
+                           breaks = seq(-1, 1, 0.5),
+                           name = 'r') +
+      theme(legend.position = 'none')
+    
+    if ((x == 1 | x == 2) & length(estaciones) == 4) {
+      plot_estacion[[x]] <- plot_estacion[[x]] +
+        theme(axis.title.x = element_blank(), 
+              axis.text.x = element_blank())  
+    } 
+    
+    if ((x == 2 | x == 4) & length(estaciones) == 4) {
+      plot_estacion[[x]] <- plot_estacion[[x]] +
+        theme(axis.title.y = element_blank(),  
+              axis.text.y = element_blank())
+    }
+    
+  }
+  
+  if (x == 2) {
+    combined_plot <- plot_estacion[[1]] + plot_estacion[[2]] + 
+      plot_layout(ncol = 2) + theme(plot.margin = margin(0, 0, 0, 0))
+  } else if (x == 4) {
+    combined_plot <- plot_estacion[[1]] + plot_estacion[[2]] + 
+      plot_estacion[[3]] + plot_estacion[[4]] + plot_layout(ncol = 2) + 
+      theme(plot.margin = margin(0, 0, 0, 0))
+    }
+  
+  ggsave(glue::glue('output/fig/analisis/correlacion_general/cor_shac_{shacs[i]}.png'), 
+         width = 12, height = 12)
+  
+}
+
 
 # correlacion DI
 
@@ -58,7 +142,7 @@ data_cor <- data |>
 
 write_rds(data_cor,'data/processed/rds/correlacion_swei.rds')
 
-# graficar correlacion DI
+# graficar correlacion SWEI
 
 data_cor <- read_rds('data/processed/rds/correlacion_swei.rds')
 
